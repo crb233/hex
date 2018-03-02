87d4e998f6d88cf6c096f4c7e21b5390b3000bf0
Curtis Bechtel <crb233@gmail.com>
2018-03-02 18:56:55 -0500

    bug fixes and better homepage functionality

--- src/public/js/home.js
+++ src/public/js/home.js
@@ -1,16 +1,34 @@
 
 function scroll(id) {
-    return function() {
-        $("#center, #content").animate({
-            scrollTop: $("#" + id).offset().top
-        }, 500, "swing");
-    };
+    $("#content").animate({
+        scrollTop: $("#" + id).offset().top
+    }, 500, "swing");
+}
+
+function setSubMenu(id) {
+    $("#sub-menu").children().appendTo("html");
+    $("#" + id).appendTo("#sub-menu");
 }
 
 $(document).ready(function() {
-    set_hex_scale(80);
-    $(".back-button").click(scroll("main-menu"));
-    $("#new-game-button").click(scroll("new-game-menu"));
-    $("#join-game-button").click(scroll("join-game-menu"));
-    $("#help-button").click(scroll("help-menu"));
+    set_hex_scale(100);
+    
+    $(".back-button").click(function() {
+        scroll("main-menu");
+    });
+    
+    $(".new-game-button").click(function() {
+        setSubMenu("new-game-menu");
+        scroll("sub-menu");
+    });
+    
+    $(".join-game-button").click(function() {
+        setSubMenu("join-game-menu");
+        scroll("sub-menu");
+    });
+    
+    $(".help-button").click(function() {
+        setSubMenu("help-menu");
+        scroll("sub-menu");
+    });
 });
